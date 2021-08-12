11db89509143de12547efd78466e9d49892215c3
Max <bigin.maks@gmail.com>
2021-08-13 00:03:53 +0300

    db correct save

--- DataBaseSync/models.py
+++ DataBaseSync/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Model for create db
 class Member(models.Model):
     name = models.CharField(max_length=255, verbose_name="Name")
-    username = models.CharField(max_length=255, verbose_name="Username")
+    username = models.CharField(max_length=255, unique=True, db_index=True, verbose_name="Username")
     description = models.CharField(max_length=255, verbose_name="Description")
     followers = models.CharField(max_length=255, verbose_name="Followers")
     following = models.CharField(max_length=255, verbose_name="Following")
@@ -11,4 +11,24 @@ class Member(models.Model):
     location = models.CharField(max_length=255, verbose_name="Location")
     repositories = models.CharField(max_length=255, verbose_name="Repositories")
     contributions = models.CharField(max_length=255, verbose_name="Сontributions in the last year")
-    profile_link = models.CharField(max_length=255, verbose_name="Profile link")
+    profile_link = models.CharField(max_length=255, unique=True, db_index=True, verbose_name="Profile link")
+
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        member = type(self).objects.filter(id=self.id)
+
+        if len(member) > 0:
+            member.update(
+                name=self.name,
+                description=self.description,
+                followers=self.followers,
+                following=self.following,
+                stars=self.stars,
+                location=self.location,
+                repositories=self.repositories,
+                contributions=self.contributions,
+            )
+        else:
+            super(type(self), self).save(*args, **kwargs)
--- dbActions.py
+++ dbActions.py
@@ -5,9 +5,10 @@ class dbSaver:
 
     # A function that initially writes 
     # the name and data parameters
-    def __init__(self, name, username, description, followers,
-                following, stars, location, repositories,
-                contributions, profile_link):
+    def __init__(self, name: str, username: str, description: str,
+                followers: str, following: str, stars: str, 
+                location: str, repositories: str, contributions: str,
+                profile_link: str):
 
         self.name = name
         self.username = username
@@ -22,12 +23,16 @@ class dbSaver:
 
     # A function that takes parameters from __init__
     # and save data to db
-    def save(self):
+    def save(self) -> None:
         m = Member(
-                name=self.name, username=self.username,
-                description=self.description, followers=self.followers,
-                following=self.following, stars=self.stars,
-                location=self.location, repositories=self.repositories,
+                name=self.name,
+                username=self.username,
+                description=self.description,
+                followers=self.followers,
+                following=self.following,
+                stars=self.stars,
+                location=self.location,
+                repositories=self.repositories,
                 contributions=self.contributions,
                 profile_link=self.profile_link
             )
